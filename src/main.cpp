17dc6c11c601ef7ea060d44c48aa8a405d8055d6
Ryledra <vince@nadin.one>
2020-09-15 12:20:48 +0100

    GLFW starter code added to engineMain.h - not compiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "engine/engineMain.h"
 
 int main()  {
+    Engine engine = Engine();
+    engine.createWindow();
     std::cout << "Hello World" << std::endl;
     return 0;
 }
