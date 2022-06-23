6ded3f97606dd5a8a3b5fa5072449d791d9570e8
Vince Martin <vince@nadin.one>
2022-06-23 09:00:35 +0100

    removed function definitions from engineMain.h and created engineMain.cpp for this.

---
+++ src/engine/engineMain.cpp
@@ -0,0 +1,51 @@
+#include "engineMain.h"
+
+Engine::Engine(const char* name, int size[2])	{
+    window_name = name;
+    window_size[0] = size[0];
+    window_size[1] = size[1];
+}
+
+int Engine::createWindow()	{
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(window_size[0], window_size[1], window_name, NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, window_size[0], window_size[1]);
+
+    /* Initialise glad */
+    gladLoadGL();
+
+    return 0;
+}
+
+void Engine::processInput()	{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+GLFWwindow* Engine::getWindow()	{
+    return window;
+}
--- src/engine/engineMain.h
+++ src/engine/engineMain.h
@@ -14,55 +14,13 @@ class Engine	{
 		GLFWwindow* window;
 
 	public:
-		Engine(const char* name, int size[2])	{
-			window_name = name;
-			window_size[0] = size[0];
-			window_size[1] = size[1];
-		}
+		Engine(const char* name, int size[2]);
 
-		int createWindow()	{
-			/* Initialize the library */
-			if (!glfwInit())
-				return -1;
+		int createWindow();
 
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-			#ifdef __APPLE__
-			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-			#endif
+		void processInput();
 
-			/* Create a windowed mode window and its OpenGL context */
-			window = glfwCreateWindow(window_size[0], window_size[1], window_name, NULL, NULL);
-			if (!window)
-			{
-				glfwTerminate();
-				return -1;
-			}
-			/* Make the window's context current */
-			glfwMakeContextCurrent(window);
-
-			if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-				std::cout << "Failed to initialize OpenGL context" << std::endl;
-				return -1;
-			}
-
-			glViewport(0, 0, window_size[0], window_size[1]);
-
-			/* Initialise glad */
-			gladLoadGL();
-
-			return 0;
-		}
-
-		void processInput()	{
-			if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-				glfwSetWindowShouldClose(window, true);
-		}
-
-		GLFWwindow* getWindow()	{
-			return window;
-		}
+		GLFWwindow* getWindow();
 };
 
 #endif
